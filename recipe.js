cd878145bad8f29bb8745056cc44ad0a8d5bee18
Daniyal Mushtaq <daniyal.musht@gmail.com>
2023-04-16 03:11:21 +0500

    add searchfun function

--- recipe.js
+++ recipe.js
@@ -1,5 +1,26 @@
 // IIFE Imediately invoke function expressions
 
-(async function() {
-    
-});
+(async function () {
+    const response = await fetch("./recipes.json");
+    const recipes = await response.json();
+
+    const IpElem = document.getElementById("searchInput");
+    const btnElem = document.getElementById("searchBtn");
+    const result_ul = document.getElementById("recipe-list");
+
+    function showRecipes(results){
+        result_ul.innerHTML = "";
+        const add_li = 
+    }
+
+    function searchfun(){
+        const search_val = IpElem.value;
+        const results = recipes.filter((recipe) =>{
+            return recipe.Name.toLowerCase().includes(search_val) || 
+            recipe.Description.toLowerCase().includes(search_val);
+        });
+        
+        showRecipes(results);
+    }
+    btnElem.addEventListener("click", searchfun);
+})();
